303bf1e451ad6db81eb16cb6b3c73bd41d4a5fbe
Vladysuk <vladik1543u7@gmail.com>
2021-05-18 14:36:22 +0300

    Delete CLR for App

--- src/main/java/ua/lviv/iot/App.java
+++ src/main/java/ua/lviv/iot/App.java
@@ -1,6 +1,5 @@
 package ua.lviv.iot;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,15 +9,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @ComponentScan({"ua.lviv.iot.service", "ua.lviv.iot.domain", "ua.lviv.iot.controller", "ua.lviv.iot.config"})
 @EnableJpaRepositories({"ua.lviv.iot.repository"})
-public class App implements CommandLineRunner {
+public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
-
-    @Override
-    public void run(String... args) throws Exception {
-   	System.out.println("Running Spring Boot Application");
-    }
-
 
 }
